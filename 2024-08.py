7a22dd3872e028edcf7103af1bfd57fb0587f12b
Krakj <krakj@krakj.krakj>
2024-12-09 23:32:12 +0100

    Day 8 and 9 add comments

--- 2024-08.py
+++ 2024-08.py
@@ -1,6 +1,7 @@
 with open('data/2024-08.txt', 'r') as f:
     d = f.read()
 
+# Create a dict with key representing the location on the map, and the value representing the field content
 G = {complex(i,j): c for i, r in enumerate(d.split('\n')) for j, c in enumerate(r)}
 m = set()
 o = set()
@@ -8,12 +9,18 @@ for x, v in G.items():
     if v != '.':
         for y, z in G.items():
             if v == z and x != y:
-                m.add(y-(x-y)) if G.get(y-(x-y)) else None
+                # (x-y) gives the 'step size' to from x to y
+                # y-stepsize then moves that step away from y
+                # G.get tests if the location f is on the grid
+                f = y-(x-y)
+                m.add(f) if G.get(f) else None
                 r = 1
-                while G.get(y-(x-y)*r):
-                    print(x, y, y-(x-y)*r)
-                    o.add(y-(x-y)*r)
+                while G.get(f):
+                    o.add(f)
                     r+=1
+                    f = y-(x-y)*r
+                # finally add the position of x itself
+                # (note that this happens inside the second loop to ensure the value isn't the only one of its kind)
                 o.add(x)
                 
 print("Part 1: ", len(m))
--- 2024-09.py
+++ 2024-09.py
@@ -6,37 +6,43 @@ with open('data/2024-09.txt', 'r') as f:
 files = list(map(int, d[::2]))
 free = list(map(int, d[1::2]))
 
+# Create a list of idid...idid like in the example
 o = []
 for i, f in enumerate(files):
     o.extend([i]*f)
     o.extend((free[i])*['.'] if not len(free) == i else [])
 
+# get the ids in reverse order
 r = [v for v in o[::-1] if v != '.']
 lr = len(r)
 
+# replace every . with the first value from the reverse ids
+# finally remove values from the right side of the list for all inserted values
 m = [r.pop(0) if  v == '.' else v for v in o][:-(lr-len(r))]
 
-print("Part 1: ", sum([i*v for i, v in enumerate(m)])) # 6241633730082
+print("Part 1: ", sum([i*v for i, v in enumerate(m)]))
 
 # Part 2:
+# Create a dict of id: spaces taken
 m = {i: f for i, f in enumerate(files)}
 
+# Go over it in reverse
 for id, v in tqdm(sorted(m.items(), reverse=True)):
     n = 0
-    for i,r in enumerate(free):
-        if v <= r:
-            b = [oi for oi, ov in enumerate(o) if ov == '.'][n]
-            if b < o.index(id):
-                o = ['.' if z == id else z for z in o]
-                o[b:b+v] = [id]*v
+    for i,r in enumerate(free): # for each free space
+        if v <= r: # if it has enough space
+            b = [oi for oi, ov in enumerate(o) if ov == '.'][n] # get the first index of the free space
+            if b < o.index(id): # make sure the free space is after the original space
+                o = ['.' if z == id else z for z in o] # replace the old space with dots
+                o[b:b+v] = [id]*v # insert the id in the new spaces
                 
-                free[i] = r-v
-                try:
+                free[i] = r-v # update the list of free spaces
+                try: # remove empty spaces from the list of free spaces (optional, helped with debugging)
                     free.remove(0)
                 except:
                     pass
             break
-        else:
+        else: # update count of how many dots have been passed
             n += r
     
 print("Part 2: ", sum([i*v for i, v in enumerate(o) if v != '.']))
